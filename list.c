24cc618cacbd9ebe2e5b2afc6e6b5558c9498649
Your Name <you@example.com>
2024-03-30 15:36:51 +0000

    new try

--- list.c
+++ list.c
@@ -102,6 +102,7 @@ void pushCurrent(List *list, void *data) {
   
         newNode->next = list->current->next;
         newNode->prev = list->current;
+        list->current->next = newNode;
         if (list->current == list->tail) {
             // Si el nodo actual es la cola, actualiza la cola al nuevo nodo
             list->tail = newNode;
